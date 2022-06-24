b2c4c996672641a52c92ad88d7dcb311e5a4151a
Sena <Sena@LAPTOP-KB17KV9I>
2022-06-25 01:24:13 +0300

    Add connsole_app

---
+++ Hi-Code/CSharp/FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class FirstConsoleApp
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hoşgeldiniz!");
+            Console.WriteLine("İsminizi giriniz:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name);
+
+        }
+    }
+}
