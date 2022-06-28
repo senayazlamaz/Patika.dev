b6a4b392125fe0f6f19e9e210c8bee86f6bf55e3
Sena <Sena@LAPTOP-KB17KV9I>
2022-06-29 01:11:40 +0300

    Add main method

---
+++ Hi-Code/CSharp/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("İsminizi giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
